32e891f34c4df53caa1fc32cd5b412d008a4462f
YuriiTitov <yurii.titov@student.uj.edu.pl>
2022-01-28 12:26:55 +0100

    Add 4_2 task

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW04_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wpisz wartość liczby N : ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Wpisz wartość liczby M : ");
+            int M = Convert.ToInt32(Console.ReadLine());
+
+            Random rnd = new Random();
+
+            List<List<int>> collection = Enumerable.Range(1, N)
+                .Select(x => Enumerable.Range(1, M).Select(a => rnd.Next(100)).ToList())
+                .ToList();
+
+            Console.WriteLine("Elementy listy list : ");
+
+            collection.SelectMany(x => { 
+                          x.Select( a => { Console.Write(a + " "); return a; }).ToList() ; 
+                          Console.WriteLine(); return x; }).ToList();
+
+
+            Console.WriteLine("Suma elementów listy list =  " + collection.Select(x => x.Sum()).Sum());
+            
+            Console.ReadKey();
+        }
+    }
+}
