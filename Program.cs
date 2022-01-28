70215b65e62bc1a84a5fca9bbfa67e9143bf14d5
YuriiTitov <yurii.titov@student.uj.edu.pl>
2022-01-28 12:23:22 +0100

    Add 4_1 task

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW04_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wpisz wartość liczby N : ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            var collection = Enumerable.Range(1, N)
+                .Where(x => ((x % 2 != 0 || x % 7 == 0) && x != 5 && x != 9))
+                .Select(x => x*x);
+
+            Console.WriteLine("Suma wszystkich elementów kolekcji = " + collection.Sum());
+            Console.WriteLine("Liczba wszystkich elementów kolekcji = " + collection.Count());
+            Console.WriteLine("Pierwszy element kolekcji = " + collection.First());
+            Console.WriteLine("Ostatni element kolekcji = " + collection.Last());
+
+            if (collection.Count() >= 3) Console.WriteLine("Trzeci element kolekcji = " + collection.ElementAtOrDefault(2));
+            else Console.WriteLine("Kolekcja nie ma trzeciego elementu");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
